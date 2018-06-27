deb15129d047c387a0d540eda7aa295c879675c8
Bijeesh <bijeesh.vm88@gmail.com>
2018-06-27 10:29:50 +0530

    Initial Commit

---
+++ NewBitbucketFile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewBitbucketFile
+//
+//  Created by BIJEESH on 27/06/18.
+//  Copyright © 2018 proaims. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
